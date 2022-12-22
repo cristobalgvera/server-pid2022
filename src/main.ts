1ec1d27021b1423ae20155f06558c52177774da0
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-21 21:25:58 -0300

    feat(open-api): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { EnvironmentService } from '@core/environment';
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +15,9 @@ async function bootstrap() {
   );
 
   const environmentService = app.get(EnvironmentService);
+
+  if (environmentService.isSwaggerEnabled) enableSwagger(app);
+
   const port = environmentService.getEnvironmentValue('PORT');
   const logger = app.get(Logger);
 
@@ -26,4 +30,16 @@ async function bootstrap() {
     logger.log(`Server listening on ${address}`, 'Bootstrap');
   });
 }
+
+function enableSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Calculator Endpoints')
+    .setDescription('The API description of PID2022 project')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+}
+
 bootstrap();
