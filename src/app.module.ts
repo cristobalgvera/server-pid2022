8037001d8e6e04e166792f71f0ea4501692f8674
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-21 21:30:08 -0300

    refactor(app): remove useless controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { EnvironmentModule } from '@core/environment';
 import { Logger, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [EnvironmentModule],
-  controllers: [AppController],
-  providers: [AppService, Logger],
+  providers: [Logger],
 })
 export class AppModule {}
