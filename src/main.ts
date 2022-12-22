b0b266fa179acc6ad0f62c1f476f2ca73c1a1fe3
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-21 21:18:31 -0300

    chore(app): use fastify as server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { EnvironmentService } from '@core/environment';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const environmentService = app.get(EnvironmentService);
+  const port = environmentService.getEnvironmentValue('PORT');
+  const logger = app.get(Logger);
+
+  await app.listen(port, '0.0.0.0', (error, address) => {
+    if (error) {
+      logger.error(error.message, error.stack, 'Bootstrap');
+      return;
+    }
+
+    logger.log(`Server listening on ${address}`, 'Bootstrap');
+  });
 }
 bootstrap();
